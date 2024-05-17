ef3cc01fcf107fc440412f19520e3ff8c46d8a43
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-05-17 00:37:25 -0700

    Se añaden las cards

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Card } from "./components/Card";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const rs = await fetch('./stays.json');
+      const rsJson = await rs.json();
+      console.log(rsJson)
+      setData(rsJson);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className='container'>
+        {data.map((e, index) => (
+          <Card
+            key={index}
+            city={e.city}
+            country={e.country}
+            superHost={e.superHost}
+            title={e.title}
+            rating={e.rating}
+            maxGuests={e.maxGuests}
+            type={e.type}
+            beds={e.beds}
+            photo={e.photo}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
